dcbd6440ba03a7d6156dd692bd854674d9db390c
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-03-02 18:35:25 -0600

    part 3 complete

--- script.js
+++ script.js
@@ -7,18 +7,30 @@ function boxMaker(color) {
   div.style.backgroundColor = color;
   return div;
 }
+// Create an array of 3 random values 0-255 that create an rgba string with a opacity argument that defaults to 1
+function colorRandomizer(opacity) {
+  opacity = opacity || 1;
+  let randColors = [];
+  for (let i = 0; i <= 3; i++) {
+    randColors.push(Math.floor(Math.random() * 255));
+  }
+  return `rgba(${randColors[0]},${randColors[1]},${randColors[2]},${opacity})`;
+}
 
 // Append the body with the created box x*9 amount of times with argument for desired number of rows
 function boardMaker(numRows) {
   numRows *= 9;
+  let opacityStep = (100 / numRows) * 0.01;
+  let opacity = opacityStep;
   for (let i = 0; i < numRows; i++) {
     if (i % 2 === 0) {
-      let box = boxMaker("red");
+      let box = boxMaker(colorRandomizer(opacity));
       document.body.append(box);
     } else {
-      let box = boxMaker("black");
+      let box = boxMaker(colorRandomizer(opacity));
       document.body.appendChild(box);
     }
+    opacity += opacityStep;
   }
 }
 
